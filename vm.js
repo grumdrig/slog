ed3b6c6eb8a2bf0a87528fb896f986f38b04cce7
Eric Fredricksen <efredricksen@gmail.com>
2022-10-12 19:12:41 -0700

    Map editor and a simple script

--- vm.js
+++ vm.js
@@ -6,16 +6,12 @@
 // Probably should have stack grow downward since mem size is to be fixed
 // (so SP should be descending in the below)
 
-// addressing modes:
-const MODE_ANY = 0;        // immediate or stack, the default
-const MODE_IMMEDIATE = 1;  // only immediate mode is supported
-
 const opcodes = [
-  { opcode: 0xC, mnemonic: 'const', mode: MODE_IMMEDIATE },
+  { opcode: 0xC, mnemonic: 'const' },
   // put a value on the stack
   // const X
   // ... => ... X ; SP += 1
-  { opcode: 0xD, mnemonic: 'sethi', mode: MODE_IMMEDIATE },
+  { opcode: 0xD, mnemonic: 'sethi' },
   // set high 8 bits of top of stack
   // sethi X
   // ... S => ... ((X << 8) | (S & 0xFF))
@@ -104,8 +100,8 @@ const opcodes = [
   // cast N : ... => ...
   // This may affect some memory location(s)
 
-  { opcode: 0x34, mnemonic: 'journal' },
-  // add ascii character to journal
+  { opcode: 0x34, mnemonic: 'log' },
+  // add ascii character to log/journal
 ];
 
 const UNARY_OPERATORS = {
@@ -116,27 +112,108 @@ const UNARY_OPERATORS = {
   0x4E9: { mnemonic: 'neg', operation: x => -x },
 };
 
+// Machine registers
+const SPECIAL_PC = 0x1;   // Program counter register
+const SPECIAL_SP = 0x2;   // Stack pointer register
+const SPECIAL_AUX = 0x3;  // Auxilliary register, gets some results
+const SPECIAL_CLOCK = 0x4;  // Counts clock cycles from startup
+
+// Environment
+const SPECIAL_TERRAIN = 0x10;
+const SPECIAL_SURROUNDINGS = 0x11;
+const SPECIAL_RESOURCE_TYPE = 0x12;  // type of visible lootable thing
+const SPECIAL_RESOURCE_QTY = 0x13;
+
+// Nearby mob
+const SPECIAL_MOB_SPECIES = 0x20;  // from some list.
+const SPECIAL_MOB_LEVEL = 0x21;
+const SPECIAL_MOB_AGGRO = 0x;  // -8 = friendly, 0 = neutral, 8 = hostile
+const SPECIAL_MOB_HEALTH = 0x;  // as %
+const SPECIAL_MOB_JOB = 0x;  // for NPC, and more generally
+const SPECIAL_MOB_ = 0x;
+
+// Character sheet
+
+const SPECIAL_LEVEL = 0x30;
+const SPECIAL_STR = 0x31;
+const SPECIAL_DEX = 0x32;
+const SPECIAL_CON = 0x33;
+const SPECIAL_INT = 0x34;
+const SPECIAL_WIS = 0x35;
+const SPECIAL_CHA = 0x36;
+
+const SPECIAL_HP = 0x40;
+const SPECIAL_HP_MAX = 0x41;
+const SPECIAL_MP = 0x42;
+const SPECIAL_MP_MAX = 0x43;
+const SPECIAL_ENCUMBRANCE = 0x44;
+const SPECIAL_ENCUMBRANCE_MAX = 0x45;
+
+const SPECIAL_INVENTORY0 = 0x50; // Gold
+const SPECIAL_INVENTORY1 = 0x51; // Reagents
+const SPECIAL_INVENTORY2 = 0x52; // Mob drops
+const SPECIAL_INVENTORY3 = 0x53; // Health potions
+const SPECIAL_INVENTORY4 = 0x54; // Mana potions
+const SPECIAL_INVENTORY5 = 0x55; // Keys
+// ...
+const SPECIAL_INVENTORY15 = 0x5F;
+
+const SPECIAL_EQUIP_WEAPON = 0x60;  // Level of puissance
+// ...
+const SPECIAL_EQUIP_SHOES = 0x6F;
+
+const SPECIAL_SPELL0 = 0x70;  // spell level
+// ...
+const SPECIAL_SPELL15 = 0x7F;
+
+const SPECIAL_LONGITUDE = 0x80;  // as fixed point?
+const SPECIAL_LATITUDE = 0x81;
+const SPECIAL_ALTITUDE = 0x82;  // 0 = on land, -1 underground, 1 flying
+const SPECIAL_FACING = 0x83;  // degrees on compass
+
+const SPECIAL_ALLEGIANCE0 = 0x90;  // ASCII codes of some arbitrary shit
+// ...
+const SPECIAL_ALLEGIANCE15 = 0x9F;
+
+const SPECIAL_TONE_FREQUENCY = 0xA0;
+const SPECIAL_TONE_VOLUME = 0xA1;
+// Room for polyphony
+
+
 class VirtualMachine {
   memory = new Array[4096].fill(0);
   special = new Array[256].fill(0);  // negative memory
 
-  get pc() { return special[1] }
-  set pc(v) { special[1] = v }
+  get pc() { return special[SPECIAL_PC] }
+  set pc(v) { special[SPECIAL_PC] = v }
 
-  get sp() { return special[2] }
-  set sp(v) { special[2] = v }
+  get sp() { return special[SPECIAL_SP] }
+  set sp(v) { special[SPECIAL_SP] = v }
 
-  get remainder() { return special[3] }
-  set remainder_fractional(f) { special[3] = f * 0x7fff }  // TODO insure 0 <= f <= 1
+  get aux() { return special[SPECIAL_AUX] }
+  get aux() { return special[SPECIAL_AUX] }
+  set aux_fractional(f) { special[SPECIAL_AUX] = f * 0x7fff }  // TODO insure 0 <= f <= 1
 
   get top() { return this.fetch(this.sp) }
   set top(v) { this.store(this.sp, v) }
 
-  store(a, v) { this.memory[a] = v }  // TODO: get bit size and signs right
+  store(a, v) {
+    // TODO: get bit size and signs of v right
+    // TODO: don't allow setting of most specials
+    if (a >= 0) this.memory[a] = v;
+    else this.special[-a] = v;
+  }
+
   fetch(a) { return (a < 0) ? this.special[-a] : this.memory[a] }  // TODO: and watch limits
 
   pop() { this.sp += 1; return this.memory[this.sp - 1] }
   push(v) { this.memory[this.sp -= 1] = v }
+
+  constructor(program) {
+    for (let i = 0; i < program.length; ++i) {
+      this.memory[i] = program[i];
+    }
+  }
 
   step() {
     const instruction = memory[pc()];
@@ -162,7 +239,12 @@ class VirtualMachine {
       this.push(this.fetch(argument));
 
     } else if (mnemonic: 'store' ) {
-      this.store(argument, pop());
+      let value = pop();
+      let address = argument;
+      if (address >= 0 ||
+         [SPECIAL_PC, SPECIAL_SP, SPECIAL_AUX, SPECIAL_FACING].includes(-address)) {
+        this.store(address, pop());
+      } // else illegal
 
     } else if (mnemonic === 'branch') {
       // with immediate addressing mode, the value is an offset not an absolute
@@ -203,13 +285,13 @@ class VirtualMachine {
     } else if (mnemonic === 'atan2') {
       let a = Math.atan2(argument, this.pop()) * 180 / Math.PI;
       this.push(Math.floor(a));
-      this.remainder_fractional = a - this.top;
+      this.aux_fractional = a - this.top;
 
     } else if (mnemonic === 'div') {
       const divisor = this.pop();
       this.push(Math.floor(argument / divisor));
       // TODO make sure to use mathematical modulus
-      this.remainder = argument % divisor;
+      this.aux = argument % divisor;
 
     // Binary bitwise
     } else if (mnemonic === 'or') {
@@ -222,7 +304,7 @@ class VirtualMachine {
       this.push(argument ^ this.pop());
 
     } else if (mnemonic === 'shift') {
-      // TODO stash the overflow in remainder register
+      // TODO stash the overflow in aux register
       let distance = this.pop();
       this.push(distance < 0 ? argument << distance : argument >> distance);
 
@@ -269,8 +351,21 @@ class VirtualMachine {
       if (spell) result = spell.effect();
       // TODO: do something with result?
 
-    } else if (mnemonic === 'journal') {
-      this.journal.push(argument);
+    } else if (mnemonic === 'log') {
+      this.log.push(argument);
+    }
 
+    this.special[SPECIAL_CLOCK] += 1;
+  }
+
+  alive() {
+    return this.special[SPECIAL_HP] > 0 &&
+           this.special[SPECIAL_CLOCK] < 0x7FFF;
+  }
+
+  run() {
+    while (this.alive()) {
+      step();
+    }
   }
 }
